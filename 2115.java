edbfdd02e9fd7155b01776e80d2b246273f8538b
Vikas Dagur <108851539+vikasdagur369@users.noreply.github.com>
2025-03-21 18:11:12 +0530

    Update 2115.java

--- 2115.java
+++ 2115.java
@@ -1,30 +1,40 @@
+
 class Solution {
-    fun findAllRecipes(
-        recipes: Array<String>,
-        ingredients: List<List<String>>,
-        supplies: Array<String>
-    ): List<String> = HashSet<String>().apply {
-        val supplySet = supplies.toHashSet()
-        val recipeMap = HashMap<String, List<String>>()
-            .apply { recipes.indices.forEach { put(recipes[it], ingredients[it]) } }
-        val making = HashSet<String>()
 
-        fun isRecipePossible(recipe: String): Boolean {
-            if (recipe in supplySet) return true  // is supply
-            if (recipe in this) return true  // is already possible recipe
-            if (recipe in making) return false  // is recursive recipe 
-            val ingredients: List<String> = recipeMap.remove(recipe) ?: return false
-
-            making.add(recipe)
-            ingredients.forEach { if (!isRecipePossible(it)) return false } // check all ings
-
-            add(recipe)   // add possible recipe
-            return true
+    private boolean check(String Curr_Recipe, List<List<String>> ingredients,HashMap<String,Integer> Recipe_Id,HashSet<String> available,HashSet<String> visited){
+        if(available.contains(Curr_Recipe))return true;
+        int id=Recipe_Id.get(Curr_Recipe);
+        for(String cR:ingredients.get(id)){
+            if(available.contains(cR))continue;
+            if(!Recipe_Id.containsKey(cR) || visited.contains(cR))return false;
+            visited.add(cR);
+            if(check(cR,ingredients,Recipe_Id,available,visited))  available.add(cR);
+            else{
+                visited.remove(cR);
+                return false;
+            } 
         }
+        return true;
+    }
 
-        recipes.forEach {
-            making.clear()
-            isRecipePossible(it)
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+        HashMap<String,Integer> Recipe_Id=new HashMap<>();
+        int i=0;
+        for(String a:recipes){
+            Recipe_Id.put(a,i);
+            i++;
         }
-    }.toList()
+        HashSet<String> available=new HashSet<>();
+        List<String> list=new ArrayList<String>();
+        for(String a:supplies)available.add(a);
+        HashSet<String> visited=new HashSet<String>();
+        for(String s:recipes){
+            int re=Recipe_Id.get(s);
+            if(check(recipes[re],ingredients,Recipe_Id, available,visited)){
+                available.add(recipes[re]);
+                list.add(recipes[re]);
+            }
+        }
+        return list;
+    }
 }
