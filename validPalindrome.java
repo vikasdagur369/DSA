3549e2001d66b3f944973af19992bce428924f72
Vikas Dagur <108851539+vikasdagur369@users.noreply.github.com>
2025-06-06 10:41:59 +0530

    Create validPalindrome.java

---
+++ validPalindrome.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        
+        int left = 0;
+        int right = s.length()-1;
+
+        while(left < right){
+            
+            if(s.charAt(left) == s.charAt(right)){
+           
+                left++;
+                right--;
+           
+           
+            }else{
+                return isPalindrome(s,left+1,right) || isPalindrome(s,left,right-1);
+            }
+        }
+
+        return true;
+    }
+
+    boolean isPalindrome(String s, int left, int right){
+        while(left < right){
+            if(s.charAt(left) == s.charAt(right)){
+                left++;
+                right--;
+            } else return false;
+        }
+        return true;
+    }
+}
